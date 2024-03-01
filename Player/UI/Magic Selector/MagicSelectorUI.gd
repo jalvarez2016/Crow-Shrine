d82a97978d7bacc70dd634ac7ac6b16d1cadddcd
jason alvarez <jasonalvarezcoraiza@gmail.com>
2024-03-01 10:13:58 -0500

    fix aiming and remove 4th spell

--- Player/UI/Magic Selector/MagicSelectorUI.gd
+++ Player/UI/Magic Selector/MagicSelectorUI.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Magic_Selector
 
 @export var player : Player
+@export var aim_spell : Node3D
 @export var spell_spawn_position : Marker3D
 @export var spell_manager : Spell_Manager
 
