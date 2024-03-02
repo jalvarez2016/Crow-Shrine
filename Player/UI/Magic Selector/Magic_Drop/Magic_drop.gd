4795e0c8f12c727a97fae1454f2c284a4be0f836
jason alvarez <jasonalvarezcoraiza@gmail.com>
2024-03-02 06:36:18 -0500

    make rigidbody

--- Player/UI/Magic Selector/Magic_Drop/Magic_drop.gd
+++ Player/UI/Magic Selector/Magic_Drop/Magic_drop.gd
@@ -3,5 +3,7 @@ extends Area3D
 
 func _on_area_entered(area):
 	if area.is_in_group("Player"):
+		print('player')
 		area.collect_magic_drop()
-		queue_free()
+		var owner = get_parent()
+		owner.queue_free()
