98ff06cf982689dd2d334c2c4993e9cf3eb02d03
jason alvarez <jasonalvarezcoraiza@gmail.com>
2024-03-18 20:45:21 -0400

    added start screen

---
+++ Start.gd
@@ -0,0 +1,10 @@
+extends Control
+var world = preload("res://World.tscn")
+var started = false
+
+func _unhandled_input(event):
+	if !started:
+		var game = world.instantiate()
+		add_child(game)
+		self.visible = false
+		started = true
